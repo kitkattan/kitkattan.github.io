af96927ae578bd70f78e580c26f073eae697ed44
Katrina Bernice Tan <katrina.bernice.tan@obf.ateneo.edu>
2023-07-16 13:42:53 +0900

    moved components to home page from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@ import Image from 'next/image'
 import Message from '../app/components/message'
 import Header from '../app/components/header'
 import Footer from '../app/components/footer'
+import Home from '../app/pages/home'
 
-export default function Home() {
+export default function app() {
   return (
     <div>
-      <Header background = "#94B4C6" text="'#FFC977'" />
-      <h1> filler text filler text fillter text</h1>
-      <Footer />
+      <Home />
     </div>
   )
 }
---
+++ src/app/pages/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+const Home = () => {
+    return(
+        <div>
+            <Header background = "#94B4C6" text="#FFC977    " />
+            <h1> filler text filler text fillter text</h1>
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
